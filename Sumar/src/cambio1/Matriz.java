c9bce3111f45540e1144c4c4665fdc737a39e412
Miguel <maalivent@gmail.com>
2022-02-18 17:57:09 +0100

    conflicto

--- Sumar/src/cambio1/Matriz.java
+++ Sumar/src/cambio1/Matriz.java
@@ -4,7 +4,7 @@ public class Matriz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mbundo");
+		System.out.println("Hola mundo");
 	}
 
 }
